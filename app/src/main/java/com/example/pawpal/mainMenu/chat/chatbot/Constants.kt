ab049f0e7bb0000b0d10434202fd6be808d0d5bc
F1D022153_Rafa Maulana Rahman <rafamaulanarahman12@gmail.com>
2025-06-18 17:01:14 +0800

    first commit

--- app/src/main/java/com/example/pawpal/mainMenu/chat/chatbot/Constants.kt
+++ app/src/main/java/com/example/pawpal/mainMenu/chat/chatbot/Constants.kt
@@ -1,5 +1,5 @@
 package com.example.pawpal.mainMenu.chat.chatbot
 
 object Constants {
-    const val OPENAI_API_KEY = "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"
+    const val OPENAI_API_KEY = "your-api-key-here"
 }
